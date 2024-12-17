9a13b6b1a6ed19a5b738a4fd1b0b0d56c898cba6
張祝維 <chuweichang0319@gmail.com>
2024-12-17 13:43:12 +0800

    add text and animation

--- src/app/discount-bar/discount-bar.component.ts
+++ src/app/discount-bar/discount-bar.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-discount-bar',
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './discount-bar.component.html',
-  styleUrl: './discount-bar.component.css'
+  styleUrls: ['./discount-bar.component.css']
 })
 export class DiscountBarComponent {
+  // The text to scroll
+  text = 'FREE SHIPPING ON ORDER $35+';
 
+  // Dynamically create multiple copies for scrolling
+  texts: string[] = [];
+
+  constructor() {
+    // Push the same text multiple times to create an effect of continuous scrolling
+    for (let i = 0; i < 10; i++) {
+      this.texts.push(this.text);
+    }
+  }
 }
