ed5c432af0d640809f2b60e172fe3841c7c2ea4d
張祝維 <chuweichang0319@gmail.com>
2024-12-19 16:13:52 +0800

    add some other drinks

--- src/app/merchandise-list/merchandise-list.component.ts
+++ src/app/merchandise-list/merchandise-list.component.ts
@@ -13,7 +13,8 @@ export class MerchandiseListComponent {
   dir_path = 'assets/images/merchandise-list/';
   items = [
     { name: 'THC Apéro Spritz', review: 633, price:35, filename: 'AperoSpritzCloseUp-1WEB.png', filenameOnHover: 'Flyers-Cocktail-Company---Apero-Spritz-Iced-Can-Render-2024-WEB-1x1-V2.png'},
-    { name: 'THC Apéro Spritz1', review: 633, price:35, filename: 'AperoSpritzCloseUp-1WEB.png', filenameOnHover: 'Flyers-Cocktail-Company---Apero-Spritz-Iced-Can-Render-2024-WEB-1x1-V2.png'},
-
+    { name: 'THC Ol\' Fashioned', review: 633, price:35, filename: 'OlFashionedCloseUp-2WEB.png', filenameOnHover: 'Flyers-Cocktail-Company---Ol_-Fashioned-Iced-Can-Render-2024-WEB-1x1.png'},
+    { name: 'THC MarGarita', review: 633, price:35, filename: 'MargaritaCloseUp-1Web1x1V3.png', filenameOnHover: 'Flyers-Cocktail-Company---Margarita-Iced-Can-Render-2024-WEB-1x1'},
+    { name: 'THC House Cocktail flight', review: 633, price:35, filename: 'Sunset_Spritz_Family_Hero_-_4_-_WEB.png', filenameOnHover: 'Flyers_Cocktail_Co_Sunset_Spritz_THC_Can_Family_V1_-_WEB.png'},
   ];
 }
