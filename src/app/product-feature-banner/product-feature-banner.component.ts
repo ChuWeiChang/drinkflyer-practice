5578a5525cef0e6ee836f86544bbbe83606c0068
ChuWeiChang <39853288+ChuWeiChang@users.noreply.github.com>
2024-12-20 15:50:59 +0800

    finished layout (#10)

--- src/app/product-feature-banner/product-feature-banner.component.ts
+++ src/app/product-feature-banner/product-feature-banner.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-product-feature-banner',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './product-feature-banner.component.html',
   styleUrl: './product-feature-banner.component.css'
 })
 export class ProductFeatureBannerComponent {
-
+  dir_path = 'assets/images/product-feature-banner/';
+  url1 = this.dir_path + 'Flyers_RTB_Icons_For_Web.png';
+  url2 = this.dir_path +'LP_Comparison_Table-01.png';
 }
