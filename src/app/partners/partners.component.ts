10cf45ad395d0dad08ee6cb5437ff64edb99b964
ChuWeiChang <39853288+ChuWeiChang@users.noreply.github.com>
2024-12-20 21:35:46 +0800

    Implement partner (#11)
    * delete unused setting
    
    * adjust rwd
    
    * basic template finished
    
    * finished for small window
    
    * adjust sequence
    
    * adjust padding
    
    * adjust padding
    
    * apply rwd for bigger screen
    
    * apply h1 position
    
    * apply height
    
    * apply height
    
    * reduce space between partners

--- src/app/partners/partners.component.ts
+++ src/app/partners/partners.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage, UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-partners',
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    UpperCasePipe
+  ],
   templateUrl: './partners.component.html',
   styleUrl: './partners.component.css'
 })
 export class PartnersComponent {
-
+  dir_path = 'assets/images/partners/';
+  items = [
+    { id:"RollingStones", filename: 'flyers_logobank-13.png', },
+    { id:"Forbes", filename: 'flyers_logobank-11.png', },
+    { id:"InsideHook", filename: 'flyers_logobank-12.png', },
+    { id:"Bloomberg", filename: 'flyers_logobank-14.png', },
+    { id:"Gotham", filename: 'flyers_logobank-10.png', },
+  ];
 }
