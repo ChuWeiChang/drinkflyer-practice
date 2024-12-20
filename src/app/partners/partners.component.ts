4f8a4df5ad6bde2e106ef98b9048260310ed8a86
張祝維 <chuweichang0319@gmail.com>
2024-12-20 20:14:33 +0800

    adjust sequence

--- src/app/partners/partners.component.ts
+++ src/app/partners/partners.component.ts
@@ -13,10 +13,10 @@ import {NgOptimizedImage, UpperCasePipe} from '@angular/common';
 export class PartnersComponent {
   dir_path = 'assets/images/partners/';
   items = [
-    { id:"RollingStones", filename: 'flyers_logobank-10.png', },
+    { id:"RollingStones", filename: 'flyers_logobank-13.png', },
     { id:"Forbes", filename: 'flyers_logobank-11.png', },
     { id:"InsideHook", filename: 'flyers_logobank-12.png', },
-    { id:"Bloomberg", filename: 'flyers_logobank-13.png', },
-    { id:"Gotham", filename: 'flyers_logobank-14.png', },
+    { id:"Bloomberg", filename: 'flyers_logobank-14.png', },
+    { id:"Gotham", filename: 'flyers_logobank-10.png', },
   ];
 }
