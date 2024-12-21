78159f4f6f75dd5e0862c830fa946defa58ecee4
張祝維 <chuweichang0319@gmail.com>
2024-12-21 20:14:59 +0800

    finish layout for bigger view

--- src/app/secondary-hero-banner/secondary-hero-banner.component.ts
+++ src/app/secondary-hero-banner/secondary-hero-banner.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage, UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-secondary-hero-banner',
-  imports: [],
+  imports: [
+    UpperCasePipe,
+    NgOptimizedImage
+  ],
   templateUrl: './secondary-hero-banner.component.html',
   styleUrl: './secondary-hero-banner.component.css'
 })
 export class SecondaryHeroBannerComponent {
+  imgSrc = 'assets/images/secondary-hero-banner/SunsetSpritzFamilyLifestyle-WEB.png'
 
 }
