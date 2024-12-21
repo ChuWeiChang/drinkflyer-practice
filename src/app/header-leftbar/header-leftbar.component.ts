bc8fbbffdbab3a1428a08eaccd47b819179c9ea8
ChuWeiChang <39853288+ChuWeiChang@users.noreply.github.com>
2024-12-21 09:53:34 +0800

    Change the implemeatation of backdrop (#12)
    * enable closing backdrop when click on it
    
    * add animation

--- src/app/header-leftbar/header-leftbar.component.ts
+++ src/app/header-leftbar/header-leftbar.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {NgClass, NgIf} from '@angular/common';
+import {BackdropService} from '../backdrop.service';
 @Component({
   selector: 'app-header-leftbar',
   imports: [
@@ -7,17 +8,32 @@ import {NgClass, NgIf} from '@angular/common';
     NgClass
   ],
   templateUrl: './header-leftbar.component.html',
-  styleUrl: './header-leftbar.component.css'
+  styleUrl: './header-leftbar.component.css',
+  providers: [BackdropService],
 })
 export class HeaderLeftbarComponent {
   isOpen: boolean = false;
   animation: boolean = false;
+  showBackdrop: boolean = false;
+
+  constructor(private backdropService: BackdropService) {}
+
+  ngOnInit() {
+    this.backdropService.backdropState.subscribe((state) => {
+      this.showBackdrop = state;
+    });
+  }
+
   toggleSidebar() {
     this.isOpen = !this.isOpen;
-    if (this.isOpen){
-      this.animation = !this.animation;
-    } else{
-      setTimeout(() => {this.animation = !this.animation;}, 500); // 2000 milliseconds = 2 seconds
+    if (this.isOpen) {
+      this.animation = true;
+      this.backdropService.openBackdrop();
+    } else {
+      this.backdropService.closeBackdrop();
+      setTimeout(() => {
+        this.animation = false;
+      }, 500); // 500 milliseconds
     }
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {NgClass, NgIf, NgOptimizedImage} from '@angular/common';
 import {HeaderLeftbarComponent} from '../header-leftbar/header-leftbar.component';
+import {BackdropService} from '../backdrop.service';
 
 @Component({
   selector: 'app-header',
@@ -11,21 +12,34 @@ import {HeaderLeftbarComponent} from '../header-leftbar/header-leftbar.component
     NgClass
   ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+  providers: [BackdropService],
 })
 export class HeaderComponent {
+  isCartOpen: boolean = false;
+  animation: boolean = false;
   isSearch: boolean = false;
+  showBackdrop: boolean = false;
+  constructor(private backdropService: BackdropService) {}
+
+  ngOnInit() {
+    this.backdropService.backdropState.subscribe((state) => {
+      this.showBackdrop = state;
+    });
+  }
+
   toggleSearch() {
     this.isSearch = !this.isSearch; // Hides the content
   }
 
-  isCartOpen: boolean = false;
-  animation: boolean = false;
+
   toggleCart() {
     this.isCartOpen = !this.isCartOpen;
     if (this.isCartOpen){
       this.animation = !this.animation;
+      this.backdropService.openBackdrop();
     } else{
+      this.backdropService.closeBackdrop();
       setTimeout(() => {this.animation = !this.animation;}, 500); // 2000 milliseconds = 2 seconds
     }
   }
