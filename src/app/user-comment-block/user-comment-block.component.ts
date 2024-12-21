43430d5b5ecd0198cad9c3c8bfb9eec42d45df02
張祝維 <chuweichang0319@gmail.com>
2024-12-21 15:26:38 +0800

    layout define

---
+++ src/app/user-comment-block/user-comment-block.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-comment-block',
+  imports: [],
+  templateUrl: './user-comment-block.component.html',
+  styleUrl: './user-comment-block.component.css'
+})
+export class UserCommentBlockComponent {
+
+}
--- src/app/user-comments/user-comments.component.ts
+++ src/app/user-comments/user-comments.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {UserCommentBlockComponent} from '../user-comment-block/user-comment-block.component';
 
 @Component({
   selector: 'app-user-comments',
-  imports: [],
+  imports: [
+    UserCommentBlockComponent
+  ],
   templateUrl: './user-comments.component.html',
   styleUrl: './user-comments.component.css'
 })
 export class UserCommentsComponent {
-
+  commentBlocks = [
+    {user:"Gwen B.", comment:"asssssssssssssssssssasssssssssssssasssssss"},
+    {user:"Gwen B.", comment:"asssssssssssssssssssasssssssssssssasssssss"},
+    {user:"Gwen B.", comment:"asssssssssssssssssssasssssssssssssasssssss"},
+    {user:"Gwen B.", comment:"asssssssssssssssssssasssssssssssssasssssss"},
+  ]
 }
