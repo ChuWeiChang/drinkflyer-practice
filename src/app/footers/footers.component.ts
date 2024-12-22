f4651e10e9047ce09dcc94f282259a3ad5028793
ChuWeiChang <39853288+ChuWeiChang@users.noreply.github.com>
2024-12-22 19:47:49 +0800

    Implement footer (#16)
    * add color for texts
    
    * define framework of footer
    
    * divide heights
    
    * finish layout for image
    
    * define height in footer-content-section
    
    * content-link-list layout finish
    
    * subscribe-section layout finish
    
    * move backdrop css to app
    
    * finished social media
    
    * font color change
    
    * key in icon
    
    * adjust layout
    
    * finish  footer-partner-section
    
    * finish  rwd for partner
    
    * rwd for footer-content-section
    
    * rwd for content-link-list
    
    * rwd for subscribe-section
    
    * rwd for social media

--- src/app/footers/footers.component.ts
+++ src/app/footers/footers.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage, UpperCasePipe} from "@angular/common";
 
 @Component({
   selector: 'app-footers',
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    UpperCasePipe,
+  ],
   templateUrl: './footers.component.html',
   styleUrl: './footers.component.css'
 })
 export class FootersComponent {
+  imgSrc:string = "assets/images/footers/Flyers_Monogram.png"
 
 }
