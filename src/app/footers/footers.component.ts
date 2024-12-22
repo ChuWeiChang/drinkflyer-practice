b872b2dd9b6306a4fd25fedefe5850eeca3d1030
張祝維 <chuweichang0319@gmail.com>
2024-12-22 14:18:22 +0800

    define framework of footer

--- src/app/footers/footers.component.ts
+++ src/app/footers/footers.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-footers',
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+  ],
   templateUrl: './footers.component.html',
   styleUrl: './footers.component.css'
 })
 export class FootersComponent {
+  imgSrc:string = "assets/images/footers/Flyers_Monogram.png"
 
 }
