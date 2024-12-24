e7fd9f0bbbf12d1c46d1d0074cfc146ad9dabad2
張祝維 <chuweichang0319@gmail.com>
2024-12-24 13:11:12 +0800

    adjust padding and margin and font size

--- src/app/product-feature-list-item/product-feature-list-item.component.ts
+++ src/app/product-feature-list-item/product-feature-list-item.component.ts
@@ -1,10 +1,11 @@
 import {Component, Input} from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
+import {NgOptimizedImage, UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-product-feature-list-item',
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    UpperCasePipe
   ],
   templateUrl: './product-feature-list-item.component.html',
   styleUrl: './product-feature-list-item.component.css'
