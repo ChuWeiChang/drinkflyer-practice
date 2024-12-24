afbbb8b44b60211055550d31a625d375d5142a82
ChuWeiChang <39853288+ChuWeiChang@users.noreply.github.com>
2024-12-24 13:18:47 +0800

    Adjust feature list layout (#22)
    * change font size for title
    
    * adjust image sequence
    
    * adjust image padding
    
    * adjust padding and margin and font size
    
    * adjust padding and margin and font size for rwd

--- src/app/product-feature-list/product-feature-list.component.ts
+++ src/app/product-feature-list/product-feature-list.component.ts
@@ -12,8 +12,8 @@ import {ProductFeatureListItemComponent} from '../product-feature-list-item/prod
 export class ProductFeatureListComponent {
   dir_path = 'assets/images/product-feature-list/';
   items = [
-    { id:1, description: 'Wind down with a high-end cocktail crafted to lift your mood without overstaying its welcome.', btnText: 'take the edge off', filename: 'Flyers_EdgeOff.png', },
-    { id:2, description: 'Enjoy anxiety-free moments with a sense of ease infused into each sip of a THC Flyers cocktail.', btnText: 'embrace the moment', filename: 'Flyers_MargCheers.png', },
-    { id:3, description: 'Elevate your drinking ritual with bartender flavors expertly crafted with 5MG of Hemp-Derived THC.', btnText: 'savor sophistication', filename: 'Flyers_Savor.png', },
+    { id:1, description: 'Wind down with a high-end cocktail crafted to lift your mood without overstaying its welcome.', btnText: 'take the edge off', filename: 'Flyers_Savor.png', },
+    { id:2, description: 'Enjoy anxiety-free moments with a sense of ease infused into each sip of a THC Flyers cocktail.', btnText: 'embrace the moment', filename: 'Flyers_EdgeOff.png', },
+    { id:3, description: 'Elevate your drinking ritual with bartender flavors expertly crafted with 5MG of Hemp-Derived THC.', btnText: 'savor sophistication', filename: 'Flyers_MargCheers.png', },
   ];
 }
